1aef9322dceb01b840aad3e995c3a4acfb315cbd
marinasameh <36296815+marinasameh@users.noreply.github.com>
2018-02-22 22:38:48 +0200

    Add files via upload

---
+++ led_1.ino
@@ -0,0 +1,13 @@
+int led_1=4 ;
+void setup() {
+  // initialize digital pin led_1 as an output. 
+  pinMode ( led_1, OUTPUT ) ;
+}
+// the loop function runs over and over again forever 
+void loop () {
+  digitalWrite (led_1, HIGH );
+  delay ( 1200 ) ;
+  digitalWrite ( led_1 , LOW );
+  delay (1200) ; 
+}
+
